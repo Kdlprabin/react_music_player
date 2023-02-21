0bb2a7d03cf37aa6f6df53a67336beeddf3bc859
prabin <kdlprabin444@gmail.com>
2023-02-21 23:23:48 +0545

    ui configuration and colors

--- src/components/controller/index.jsx
+++ src/components/controller/index.jsx
@@ -4,7 +4,7 @@ import Progressbar from './Progressbar'
 import MainController from './mainController'
 function index() {
   return (
-    <VStack backgroundColor={'gray'} textColor={'white'} position={'fixed'} bottom={'10'} gap={'2'} width={'100%'}>
+    <VStack borderTop={'2px solid #341a3e'} paddingBottom={'10px'} backgroundColor={'#2a2826'} textColor={'white'} position={'fixed'} bottom={'0'} gap={'2'} width={'100%'}>
       <Progressbar/>
       <MainController />
     </VStack>
--- src/components/mainpage/index.jsx
+++ src/components/mainpage/index.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
-import {VStack, HStack , Text} from '@chakra-ui/react'
+import { VStack, HStack, Text, Box } from '@chakra-ui/react'
+
+function MediaItem() {
+  return (
+    <VStack>
+      <Box width={"200px"} height={'200px'} borderRadius={'5px'} border={'1px solid #aeabaa'} backgroundColor={'#1b1a1a'}>
+      </Box>
+      <Text>Name</Text>
+    </VStack>);
+}
+
+
 function index() {
   return (
-    <VStack  backgroundColor={'#525252'}>
-      <Text>Home</Text>
+    <VStack backgroundColor={'#2a2826'} width={'100%'} height={'100%'} alignItems={'Left'} paddingLeft={'50'} overflow={'scroll'} overscroll={'contain'} gap={'10'}>
+      <Text
+        fontSize={'40px'}
+      >Home</Text>
+      <Text
+        fontSize={'24px'}
+      >Recent Media</Text>
+      <HStack gap={'10'}>
+        <MediaItem />
+        <MediaItem />
+        <MediaItem />
+        <MediaItem />
+        <MediaItem />
+      </HStack>
+      <HStack gap={'10'}>
+        <MediaItem />
+        <MediaItem />
+        <MediaItem />
+        <MediaItem />
+        <MediaItem />
+      </HStack>
+      <HStack gap={'10'}>
+        <MediaItem />
+        <MediaItem />
+        <MediaItem />
+        <MediaItem />
+        <MediaItem />
+      </HStack>
     </VStack>
   )
 }
